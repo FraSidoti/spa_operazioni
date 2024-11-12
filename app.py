e3ff5281db2a1fbc8820ad2d7696ca0e71c9872b
FraSidoti Sidoti <sidoti.francesco@iisgalvanimi.edu.it>
2024-11-12 11:49:37 +0000

    server API completato

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app=Flask(__name__)
+
 # server web - codice per restituire la home page
 @app.route("/")
 def homepage():
@@ -8,7 +9,25 @@ def homepage():
 # server API
 @app.route("/calcola", methods=["POST"])
 def calcola():
-    dati = request.get_json()
+    # prendo le info dal front end
+    json_data = request.get_json()
+    if json_data:
+        num1 = json_data.get('num1')
+        num2 = json_data.get('num2')
+        operazione = json_data.get('operazione')
+        # elaborazione delle info
+        if operazione == 'addizione':
+            risultato = num1 + num2
+        elif operazione == 'sottrazione':
+            risultato = num1 + num2
+        elif operazione == 'moltiplicazione':
+            risultato = num1 * num2
+        elif operazione == 'divisione':
+            risultato = num1 // num2
+        # restituire i risultati al front end
+        return jsonify(risultato = risultato)
+    else:
+        return jsonify(risultato = 'mancano i dati')
 
 if __name__== "__main__":
-    app.run(host="0.0.0.0", port=3245, debug=True)
+    app.run(host="0.0.0.0", port=3246, debug=True)
